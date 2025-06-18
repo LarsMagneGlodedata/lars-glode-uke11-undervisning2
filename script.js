46edeb0203e7684ff153f3826c5ad3bc660eb23d
LarsMagneGlodedata <larsmagne.glodedata@gmail.com>
2025-06-18 11:00:23 +0200

    mmm

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("Hallajen");
+const factsList = document.getElementById('factsList')
+
+// Funksjon som henter og tar i mot data fra en Cat Facts API
+
+async function fetchFacts() {
+    const fetchData = await fetch('https://catfact.ninja/facts')
+    const parse = await fetchData.json()
+    return parse
+}
+
+async function displayFacts() {
+    const response = await fetchFacts()
+    console.log(response)
+ 
+    const catFacts =  response.data
+    console.log(catFacts)
+
+    catFacts.forEach(item => {
+        const li = document.createElement('li')
+
+        const factItem = item.fact
+        li.textContent = factItem
+
+        factsList.appendChild(li)
+    });
+
+}
+
+displayFacts()
